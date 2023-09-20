7cf2b2c760fa5e38d12c832e86a07bdf0cb157f2
godswill9 <ghycinth9@gmail.com>
2023-09-20 04:50:14 -0700

    promises

--- routes/chatPremium.js
+++ routes/chatPremium.js
@@ -1,40 +1,44 @@
 const express = require("express");
 const route = express.Router();
+const fetch = require("node-fetch");
 
 const conversation = [];
+
 route.post("/", async (req, res) => {
-  var url = "https://api.openai.com/v1/chat/completions";
-  var api_key = process.env.API_KEY;
-  conversation.push({
-    role: "user",
-    content: `always reply very simply as a mechanic ${req.body.message}`,
-  });
-  //   console.log(req.body);
-  await fetch(url, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${api_key}`,
-      "Content-Type": "application/json",
-    },
-    mode: "cors",
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: conversation,
-    }),
-  })
-    .then((res) => res.json())
-    .then((response) => {
-      conversation.push({
-        role: "assistant",
-        content: response.choices[0].message.content,
-      });
-      res.send({ data: response.choices[0].message.content });
-      // console.log(conversation);
-    })
-    .catch((err) => {
-      res.send({ data: "an error occured" });
-      throw err;
+  try {
+    var url = "https://api.openai.com/v1/chat/completions";
+    var api_key = process.env.API_KEY;
+
+    conversation.push({
+      role: "user",
+      content: `always reply very simply as a mechanic ${req.body.message}`,
     });
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${api_key}`,
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: conversation,
+      }),
+    });
+
+    const responseData = await response.json();
+
+    conversation.push({
+      role: "assistant",
+      content: responseData.choices[0].message.content,
+    });
+
+    res.send({ data: responseData.choices[0].message.content });
+  } catch (err) {
+    console.error("An error occurred:", err);
+    res.status(500).send({ data: "An error occurred" });
+  }
 });
 
 module.exports = route;
