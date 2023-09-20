0c82edf84397c5023a87ceb0296eb9bf3c01adc3
godswill9 <ghycinth9@gmail.com>
2023-09-20 04:09:04 -0700

    first commit

---
+++ routes/chatPremium.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const route = express.Router();
+
+const conversation = [];
+route.post("/", async (req, res) => {
+  var url = "https://api.openai.com/v1/chat/completions";
+  var api_key = process.env.API_KEY;
+  conversation.push({
+    role: "user",
+    content: `always reply very simply as a mechanic ${req.body.message}`,
+  });
+  //   console.log(req.body);
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${api_key}`,
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: conversation,
+    }),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      conversation.push({
+        role: "assistant",
+        content: response.choices[0].message.content,
+      });
+      res.send({ data: response.choices[0].message.content });
+      // console.log(conversation);
+    })
+    .catch((err) => {
+      res.send({ data: "an error occured" });
+      throw err;
+    });
+});
+
+module.exports = route;
---
+++ server.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const chat = require("./routes/chat");
+const chatPremium = require("./routes/chatPremium");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5500",
+      "http://127.0.0.1:5500",
+      "http://127.0.0.1:5501",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello server 2");
+});
+
+app.use("/", chat);
+app.use("/premium", chatPremium);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("app is listening");
+});
